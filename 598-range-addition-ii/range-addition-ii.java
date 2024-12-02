8f39eec5a00b7ad65db2a9049b22895654ffbaf8
Keshab Kumar <keshabkumarjha876@gmail.com>
2024-12-02 19:58:45 +0530

    Delete 598-range-addition-ii directory

--- 598-range-addition-ii/range-addition-ii.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int maxCount(int m, int n, int[][] ops) {
-        for(int[] o:ops) {
-            m = Math.min(m,o[0]);
-            n = Math.min(n,o[1]);
-        }
-        return m*n;
-    }
-}
