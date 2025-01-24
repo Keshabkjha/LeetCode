4d75237532fabdfe3e59d8bde1a9f9a6299e7b74
Keshab Kumar <keshabkumarjha876@gmail.com>
2025-01-25 00:29:13 +0530

    Time: 4 ms (99.02%), Space: 55.4 MB (84.93%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
@@ -1,39 +1,29 @@
 class Solution {
-    static boolean dfs(int cur, int graph[][],boolean visited[],boolean dfsVisited[],boolean hasCycle[]){
-        visited[cur] = true;
-        dfsVisited[cur] = true;
-        int arr[] = graph[cur];
-        for(int i : arr){
-            if(!visited[i] ){
-                if(dfs(i,graph,visited,dfsVisited,hasCycle)){
-                    hasCycle[cur] = true;
-                    return true;
-                }
-            }
-            else if (visited[i]&& dfsVisited[i]){
-                hasCycle[cur]= true;
-                return true;
-            }
-        }
-        dfsVisited[cur] = false;
-        return false;
-    }
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        int len = graph.length;
-        boolean visited[] = new boolean[len];
-        boolean dfsVisited[] = new boolean[len];
-        boolean hasCycle[] = new boolean[len];
-        for(int i =0;i<len;i++){
-            if(!visited[i]){
-                dfs(i,graph,visited,dfsVisited,hasCycle);
+        int n = graph.length;
+        int[] state = new int[n];
+        List<Integer> res = new ArrayList<>();
+
+        for(int i = 0; i < n ; i++) {
+            if(dfs(graph, i, state)) {
+                res.add(i);
             }
         }
-        List<Integer> ans = new ArrayList<>();
-        for(int i =0;i<len;i++){
-            if(!hasCycle[i]){
-                ans.add(i);
+
+        return res;
+    }
+
+    private boolean dfs(int[][] graph, int node, int[] state) {
+        if(state[node] > 0) return state[node] == 2;
+
+        state[node] = 1;
+
+        for(int next : graph[node]) {
+            if (state[next] == 1 || !dfs(graph, next, state)) {
+                return false;
             }
         }
-        return ans;
+        state[node] = 2;
+        return true;
     }
 }
