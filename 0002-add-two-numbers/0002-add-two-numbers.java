80c0682abf9b79ae0cc8ee16cb6a8af0ef2b3de0
Keshab Kumar <keshabkumarjha876@gmail.com>
2025-04-18 22:22:20 +0530

    Time: 1 ms (100%), Space: 44.7 MB (41.23%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -1,78 +1,33 @@
-
- // Definition for singly-linked list.
-  /*public class ListNode {
-      int val;
-      ListNode next;
-      ListNode() {}
-      ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-  }*/
- 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode finalNode = null;
-        Integer remainder = 0;
-        Integer division = 0;
-        Integer total = 0;
-       
-        List<Integer> firstArray = new ArrayList();
-        while(l1 != null || l2  != null || division > 0 )
-        {
-            
-            int firstValue = 0;
-            int secondValue = 0;
-           
-            if(l1 != null){
-                firstValue = l1.val;
+        ListNode resHead = new ListNode();
+        ListNode Ptr = resHead;
+        int sum = 0;
+        int carry = 0;
+        while (l1!=null || l2!=null || carry!=0) {
+            sum=carry;
+            if (l1!=null) {
+                sum+=l1.val;
+                l1=l1.next;
             }
-
-            if(l2 != null){
-               secondValue = l2.val;
+            if (l2!=null) {
+                sum+=l2.val;
+                l2=l2.next;
             }
-
-            total = firstValue + secondValue + division;
-
-            if(total < 10) {
-        
-            if(finalNode == null)
-            {
-               finalNode = new ListNode(total);
-            } else {
-                ListNode temp = finalNode;
-                while(temp != null && temp.next != null)
-               {
-                   temp = temp.next;
-               }
-        
-                temp.next = new ListNode(total);
-            }
-             division = 0;
-            } 
-            else
-            {
-            remainder = total % 10;
-            division  = total / 10;
-            if(finalNode == null)
-            {
-               finalNode = new ListNode(remainder);
-            } else {
-                ListNode temp = finalNode;
-                while(temp != null && temp.next != null)
-               {
-                   temp = temp.next;
-               }
-        
-                temp.next = new ListNode(remainder);
-            }
-            }
-             if(l1 != null){
-                 l1 = l1.next;
-            }
-            if(l2 != null){
-                l2 = l2.next;
-            }
+            carry=sum/10;
+            Ptr.next=new ListNode(sum%10);
+            Ptr=Ptr.next;
         }
-
-        return finalNode;
+        return resHead.next;
     }
 }
