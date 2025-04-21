865f7f47443a358b6222e1523c1e892a3434ef63
Keshab Kumar <keshabkumarjha876@gmail.com>
2025-04-22 00:13:01 +0530

    Time: 0 ms (100%), Space: 42.2 MB (8.62%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,35 +1,28 @@
 class Solution {
-    public List<Integer> spiralOrder(int[][] arr) {
-        List<Integer> res = new ArrayList<>();
-        int b = arr.length, r = arr[0].length;
-        int t = 0, l = 0, size = b*r;
-        while(t<=b && l<=r){
-            for (int i = l; i < r; i++) {
-                res.add(arr[t][i]);
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        int startrow = 0, endrow = matrix.length-1, startcol = 0, endcol = matrix[0].length-1;
+        while (startrow <= endrow && startcol <= endcol) {
+            for (int j = startcol; j <= endcol; j++) {
+                result.add(matrix[startrow][j]);
             }
-            t++;
-            for (int i = t; i < b; i++) {
-                res.add(arr[i][r-1]);
+            for (int i = startrow + 1; i <= endrow; i++) {
+               result.add(matrix[i][endcol]);
             }
-            r--;
-            if(res.size()>=size){
-                break;
+            for (int j = endcol - 1; j >= startcol; j--) {
+                if (startrow == endrow) break;
+               result.add(matrix[endrow][j]);
             }
-            for (int i = r-1; i >= l; i--) {
-                res.add(arr[b-1][i]);
-            }
-            b--;
-            if(res.size()>=size)
-                break;
-            for (int i = b-1; i >= t; i--) {
-                res.add(arr[i][l]);
-            }
-            l++;
-            if(res.size()>=size)
-                break;
 
+            for (int i = endrow - 1; i > startrow; i--) {
+                if (startcol == endcol) break;
+               result.add(matrix[i][startcol]);
+            }
+            startrow++;
+            endrow--;
+            startcol++;
+            endcol--;
         }
-        return res;
-        
+        return result;
     }
 }
