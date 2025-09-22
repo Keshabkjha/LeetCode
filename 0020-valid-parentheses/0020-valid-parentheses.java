9ada7427e2281a62320fbc0ec95535e226fa32ad
Keshab Kumar <keshabkumarjha876@gmail.com>
2025-09-22 20:59:34 +0530

    Time: 158 ms (8.2%), Space: 45.4 MB (5.75%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,21 +1,9 @@
 class Solution {
-    public boolean isValid(String parentheses) {
-        Stack<Character> stack = new Stack<>();
-        HashMap<Character, Character> map = new HashMap<>();
-        map.put('(', ')');
-        map.put('{', '}');
-        map.put('[', ']');
-
-        for (int i = 0; i < parentheses.length(); i++) {
-            if (map.containsKey(parentheses.charAt(i))) {
-                stack.push(parentheses.charAt(i));
-            } else if (!stack.isEmpty() && parentheses.charAt(i) == map.get(stack.peek())) {
-                stack.pop();
-            } else {
-                return false;
-            }
-        }
-
-        return stack.isEmpty();
+    public boolean isValid(String s) {
+        while (s.contains("()") || s.contains("[]") || s.contains("{}")){
+         s= s.replace("()","").replace("[]","").replace("{}","");
+     }
+     
+     return s.length()==0;
     }
 }
